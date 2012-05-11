bfa23e902219613dbaa29c673835a0d25df51024
newpavlov <newpavlov@gmail.com>
2012-05-11 23:11:56 +0400

    Improved crawler.

--- amber/crawler/samba.py
+++ amber/crawler/samba.py
@@ -8,7 +8,7 @@ SMBC_DIR = 7L
 SMBC_SERVICE = 3L
 
 def host_scanner(host, mongo_collection):
-    """ Generator to walk around SMB tree """
+    """ Scanning given host and save results in given mongo collection """
 
     mongo_collection.drop()
 
@@ -19,46 +19,49 @@ def host_scanner(host, mongo_collection):
     item_type = lambda entry: 'file' if entry.smbc_type == SMBC_FILE else 'dir'
 
     def get_metainfo(path):
-        data = ctx.open(path).fstat()
-        return 0, 1
+        data = ctx.stat(path)
+        change_time = datetime.fromtimestamp(data[8])
+        size = data[6]
+        return change_time, size
 
-    def save_object(path, dent_type, change_time, size=None):
-        data = {
+    def save_object(path, dent_type, change_time, size):
+        mongo_collection.save({
             '_id': path.replace(smb_host + '/', ''), # Path without host will be object id
             'is_f': True if dent_type == 'file' else False, # Is object has hile type
+            'sz': size,
             'ch_t': change_time,   # Object change time
             's_t': datetime.now(), # Save time
-        }
-        if size: data['sz'] = size # Size of the object if given 
-        mongo_collection.save(data)
+        })
+
 
     def process_entry(root, dent):
         try:
             dent_type = item_type(dent)
             path = root + '/' + dent.name
 
-            changed_at = None
+            sum_size = None
             if dent_type == 'dir':
+                sum_size = 0
                 for entry in ctx.opendir(path).getdents():
                     if entry.name not in ['..', '.']:
-                        entry_time = process_entry(path, entry)
+                        sum_size += process_entry(path, entry)
 
-                        if changed_at == None or changed_at < entry_time: changed_at == entry_time
+            changed_at, size = get_metainfo(path)
+            # Size of folder is sum of size of all files in it
+            if sum_size <> None: size = sum_size
+            save_object(path, dent_type, changed_at, size)
 
-                # If time is None then directory does not contain files, so we ommit it
-                if changed_at <> None:
-                    save_object(path, dent_type, changed_at)
+            return size
 
-            elif dent_type == 'file':
-                changed_at, size = get_metainfo(path)
-                save_object(path, dent_type, changed_at, size)
-            return changed_at
         except smbc.NoEntryError:
             print('No entry error: ' + path)
         except smbc.PermissionError:
             print('Permission error: ' + path)
         except:
             print('Unknown error: ' + path)
+
+        return 0
+
 
     for entry in ctx.opendir(smb_host).getdents():
         if entry.smbc_type == SMBC_SERVICE:
